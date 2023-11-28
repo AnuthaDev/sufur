06896e9361fe71f4f43f8e0424c0136327bb2620
Anurag Thakur <anuthadev@gmail.com>
2023-11-28 18:02:02 +0530

    Added libsufur support

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <cstdio>
+#include <iostream>
 
+
+extern "C" {
+#include "libsufur/libsufur.h"
+}
 
 int main(int argc, char *argv[])
 {
+    usb_drive *val = NULL;
+    int size = enumerate_usb_mass_storage(&val);
+
+    for (int i = 0;i<size;i++) {
+        printf("\n%s %s\n", val[i].vendor_name, val[i].model_name);
+        printf("%li\n", val[i].size);
+        printf("%s\n", val[i].devnode);
+    }
+    std::cout<<std::endl;
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
